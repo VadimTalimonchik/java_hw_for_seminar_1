258197bba59b63214fbb3c23e6193a3d757d17ad
VadimTalimonchik <tvadmins@gmail.com>
2023-03-27 16:34:41 +0300

    Задача 3

---
+++ hw_3.java
@@ -0,0 +1,32 @@
+// Задача 3.
+// Реализовать простой калькулятор
+
+import java.util.Scanner;
+public class hw_3 {
+   public static void main(String[] args) {
+      double num1;
+      double num2;
+      double ans;
+      char op;
+      Scanner reader = new Scanner(System.in);
+      System.out.print("Введите два числа: \n");
+      num1 = reader.nextDouble();
+      num2 = reader.nextDouble();
+      System.out.print("\nВведите операцию вычисления (+, -, *, /): ");
+      op = reader.next().charAt(0);
+      switch(op) {
+         case '+': ans = num1 + num2;
+            break;
+         case '-': ans = num1 - num2;
+            break;
+         case '*': ans = num1 * num2;
+            break;
+         case '/': ans = num1 / num2;
+            break;
+         default:  System.out.printf("Ошибка! Введите правильную операцию вычисления.\n");
+            return;
+      }
+      System.out.print("\nПолученный результат:\n");
+      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans + "\n");
+   }
+}
